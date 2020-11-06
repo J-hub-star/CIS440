5a3597e644c2c8a44fd59f8468eaeaceacbe4804
andrew3737 <49882247+andrew3737@users.noreply.github.com>
2020-11-05 21:27:53 -0600

    Update settings.py

--- techmnsu/techmnsu/settings.py
+++ techmnsu/techmnsu/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'techmnsu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'dbsqlite3'),
+#         }
+#    }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carrental_techmsu',
+        'USER': 'dbandrew',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
